f206ea9182faddd0a4d0acd492dc53bb7a3725c9
xubeiyan <yueyongdong@gmail.com>
2023-11-16 14:40:19 +0800

    feature(userDefineIngre/form.svelte): add user define ingredients

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,21 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import path from 'path';
 
+import { readFileSync } from 'fs';
+import { fileURLToPath } from 'url';
+ 
+const file = fileURLToPath(new URL('package.json', import.meta.url));
+const json = readFileSync(file, 'utf8');
+const pkg = JSON.parse(json);
+
 export default defineConfig({
 	resolve: {
 		alias: {
 			$src: path.resolve('./src'),
 		}
 	},
-	plugins: [sveltekit(), purgeCss()]
+	plugins: [sveltekit(), purgeCss()],
+	define: {
+    '__APP_VERSION__': JSON.stringify(pkg.version),
+  }
 });
